6cf61081e37f941a4ad73ae6d9577db37d5da266
ibrar11 <ibrarmunawar0gmail.com>
2026-01-13 17:25:16 +0500

    created venv and installed required libraries

---
+++ search.py
@@ -0,0 +1,13 @@
+import math
+
+import numpy as np
+import scipy
+import torch
+from sentence_transformers import SentenceTransformer
+
+documents = [
+    'Bugs introduced by the intern had to be squashed by the lead developer.',
+    'Bugs found by the quality assurance engineer were difficult to debug.',
+    'Bugs are common throughout the warm summer months, according to the entomologist.',
+    'Bugs, in particular spiders, are extensively studied by arachnologists.'
+]
